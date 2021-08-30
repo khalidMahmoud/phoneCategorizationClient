0f2f0d5d2be1937250f4deecaf9e6dc4221c422e
khalidMahmoudBasuiny <khalid.mahmoud@asset.com.eg>
2021-08-30 03:42:16 +0200

    call API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {PhoneService} from "./services/phone.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'phoneCategorizationClient';
+
+  constructor(private phoneService: PhoneService) {
+    this.getPhoneList();
+  }
+
+  getPhoneList() {
+    this.phoneService.getPhoneList().subscribe(result => {
+      console.log(result)
+      }, error => {
+
+      }, () => {
+
+      }
+    );
+  }
 }
